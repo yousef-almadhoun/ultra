64829170ab11372fc017062559dc51882cbe02a3
unknown <yousefmadhoun5@gmail.com>
2020-08-15 03:10:13 +0300

    Updates

---
+++ precache-manifest.8ca1dc9210e342071be9aa2fd1bfbcb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75a027e8d88d0748337b3bb4bbd3618",
+    "url": "/yousef-almadhoun/ultra/index.html"
+  },
+  {
+    "revision": "99854d59a99376326b01",
+    "url": "/yousef-almadhoun/ultra/static/css/main.98446af3.chunk.css"
+  },
+  {
+    "revision": "b4efe797eb3d3d0a391e",
+    "url": "/yousef-almadhoun/ultra/static/js/2.b22fa14e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yousef-almadhoun/ultra/static/js/2.b22fa14e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99854d59a99376326b01",
+    "url": "/yousef-almadhoun/ultra/static/js/main.a84ce883.chunk.js"
+  },
+  {
+    "revision": "f613e2ebf8b64087cd5e",
+    "url": "/yousef-almadhoun/ultra/static/js/runtime-main.ddd01d28.js"
+  }
+]);
